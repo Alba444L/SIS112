41e1c407e99b4f5ce0bbcfa51e43a1d1e763cc77
Alba Libera <alba.libera@ucb.edu.bo>
2024-08-12 11:44:30 -0400

    Program.cs

--- mi primer programa en c#/Program.cs
+++ mi primer programa en c#/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace mi_primer_programa_en_c_;
+﻿namespace mi_primera_programa_en_c_;
 
 class Program
 {
     static void Main(string[] args)
     {
+        //Primer metodo, mostrar un Hola Mundo
+        HelloWorld();
+
+        //Segundo metodo, mostrar la edad ingresada por el usuario
+        ObtenerEdad();
+    }
+
+    public static void HelloWorld()
+    {
         Console.WriteLine("Hello, World!");
+    }
+
+    public static void ObtenerEdad()
+    {
+        Console.WriteLine("Introduzca su edad:");
+        string? edadInput = Console.ReadLine();
+        if (string.IsNullOrEmpty(edadInput))
+        {
+            Console.WriteLine("Introduzca un valor");
+        }
+        else
+        {
+            int edadInteger = Int32.Parse(edadInput);
+            Console.WriteLine("Tu Edad es: " + edadInteger);
+        }
     }
 }
