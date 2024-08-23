3933041962257c09c109606e72fe47325e2eb5a9
Alba Libera <alba.libera@ucb.edu.bo>
2024-08-23 11:37:28 -0400

    persona java script

--- POO/py/persona.py
+++ POO/py/persona.py
@@ -1,22 +1,28 @@
 class Persona:
+    #Metodo Constructor
     def __init__(self, nombre, edad, carrera):
         self.nombre = nombre
         self.edad = edad
         self.carrera = carrera
 
+    #Metodo Saludar
     def saludar(self):
         print(f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años.")
 
+    #Metodo Cumpleaños
     def cumpleaños(self):
         self.edad += 1
         print(f"¡Feliz cumpleaños! Ahora tengo {self.edad} años.")
 
+    #Metodo Estudiar
     def estudiar(self):
         print(f"Estoy estudiando {self.carrera}")
 
 # Crear una instancia de la clase Persona
 persona1 = Persona("Juan", 20, "Medicina")
+persona2 = Persona("Maria", 21, "Odontologia")
 
 # Llamar a los metodos
 persona1.saludar()
-persona1.estudiar()
+persona2.saludar()
+#persona1.estudiar()
