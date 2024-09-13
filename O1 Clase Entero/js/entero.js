6e9eadf6e825754d2de7de5d32427a497ddb86f1
Alba Libera <alba.libera@ucb.edu.bo>
2024-09-13 11:24:00 -0400

    negativo y positivo

--- O1 Clase Entero/js/entero.js
+++ O1 Clase Entero/js/entero.js
@@ -9,7 +9,8 @@ class Entero {
 
     //CLASS- Cargar el valor de Num
     setNum(){
-        this.Num = 10;
+        const input = document.getElementById('numeroInput');
+        this.Num = parseInt(input.value, 10);
     }
 
     //CLASS- Obtener valor de Num
@@ -22,6 +23,27 @@ class Entero {
         const resultadoDiv = document.getElementById('resultado');
         resultadoDiv.textContent = this.Num;
     }
+
+    showResultado(respuesta){
+        const resultadoDiv = document.getElementById('resultado');
+        resultadoDiv.textContent = respuesta;
+    }
+
+    incrementarNum(){
+        this.Num = this.Num + 1;
+    }
+
+    decrementarNum(){
+        this.Num = this.Num - 1;
+    }
+
+    esParImpar(){
+        return(this.Num % 2 == 0)
+    }
+
+    esPositivoNegativo() {
+        return (this.Num > 0)
+    }
 }
 
 //Las funciones = button HTML
@@ -32,6 +54,33 @@ function cargarNum(){
     ClaseEntero.setNum();
 }
 
+//Mostrar el valor de Num en HTML
 function mostrarNum(){
     var valorNum = ClaseEntero.showNum();
+}
+
+//Incremetar el valor de Num
+function incrementarValor(){
+    ClaseEntero.incrementarNum();
+    ClaseEntero.showNum();
+}
+
+//Decrementar el valor de Num
+function decrementarValor(){
+    ClaseEntero.decrementarNum();
+    ClaseEntero.showNum();
+}
+
+//Verifica si es Par o Impar
+function esParImparValorNum(){
+    var respuesta = ClaseEntero.esParImpar();
+    var resp = respuesta ? "Es Num Par":"Es Num Impar"
+    ClaseEntero.showResultado(resp)
+}
+
+//Verifica si es Positivo o Negativo
+function esPositivoNegativoValorNum(){
+    var respuesta = ClaseEntero.esPositivoNegativo();
+    var resp = respuesta ? "Es Num Positivo":"Es Num Negativo"
+    ClaseEntero.showResultado(resp)
 }
