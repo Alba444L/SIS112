5f51269be9b9ec19d111432d8843a6a39f2aeaed
Alba Libera <alba.libera@ucb.edu.bo>
2024-09-27 11:34:32 -0400

    link

--- 03 CLASE LISTA ENTEROS/js/lista-enteros.js
+++ 03 CLASE LISTA ENTEROS/js/lista-enteros.js
@@ -1,1 +1,47 @@
-g
+class ListaEnteros {
+    constructor() {
+        this.lista = [];
+    }
+
+    agregar(valor) {
+        this.lista.push(valor);
+        this.actualizarLista();
+    }
+
+    actualizarLista() {
+        const listaElemento = document.getElementById('lista');
+        listaElemento.innerHTML = '';
+        this.lista.forEach(num => {
+            const li = document.createElement('li');
+            li.textContent = num;
+            listaElemento.appendChild(li);
+        });
+    }
+
+    eliminar(valor) {
+        const index = this.lista.indexOf(valor);
+        if (index !== -1) {
+            this.lista.splice(index, 1);
+        }
+    }
+
+    buscar(valor) {
+        return this.lista.indexOf(valor);
+    }
+
+    ordenar() {
+        this.lista.sort((a, b) => a - b); // Orden ascendente
+    }
+}
+
+const miLista = new ListaEnteros();
+
+document.getElementById('agregarBtn').addEventListener('click', () => {
+    const valor = parseInt(document.getElementById('numero').value);
+    if (!isNaN(valor)) {
+        miLista.agregar(valor);
+        document.getElementById('numero').value = ''; // Limpiar el campo
+    } else {
+        alert('Por favor, ingresa un número válido.');
+    }
+});
