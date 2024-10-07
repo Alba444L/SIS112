ac011ecace0b9f4f7478eb916c9d432b3958a0ce
Alba Libera <alba.libera@ucb.edu.bo>
2024-10-07 12:22:56 -0400

    cadenas

--- 03 CLASE LISTA ENTEROS/js/lista-cadenas.js
+++ 03 CLASE LISTA ENTEROS/js/lista-cadenas.js
@@ -39,6 +39,79 @@ class ListaCadenas {
 }
 
 const miLista = new ListaCadenas();
+miLista.agregar('Hola');
+miLista.agregar('Mundo');
+miLista.agregar('UCB SCZ INDUSTRIAL');
+
+/*
+// Retornar la cadena mayor caracteres
+
+//var = let
+//------------------------------Hola
+var a = miLista.lista[0]
+console.log(a);
+var aCant = a.length;
+console.log(aCant); //4
+//------------------------------Mundo
+var b = miLista.lista[1]
+console.log(b);
+var bCant = b.length;
+console.log(bCant); //5
+//------------------------------UCB
+var c = miLista.lista[2]
+console.log(c);
+var cCant = c.length;
+console.log(cCant); //3
+
+//------------------------------Busca el mayor
+var mayor = 0;
+var posicion = -1;
+if (aCant > mayor) {
+    aCant = mayor;
+    posicion = 0;
+}
+//----
+if (bCant > mayor) {
+    mayor = bCant;
+    posicion = 1;
+}
+//----
+if (aCant > mayor) {
+    mayor = cCant;
+    posicion = 2;
+}
+console.log('La mayor cantidad de caracteres tiene: ' + mayor);
+console.log('La CADENA con mayor cantidad de caracteres es: ' + miLista.lista[posicion]);
+
+
+console.log(miLista.lista); // Imprime: ['Hola', 'Mundo'm 'UCB']
+*/
+
+//REALIZAR EL MISMO EJERCICIO UTILIZANDO FOR
+let mayor = 0;
+let posicion = -1;
+
+// Recorremos la lista con un for
+for (let i = 0; i < miLista.lista.length; i++) {
+    let cadena = miLista.lista[i];
+    let longitud = cadena.length;
+
+    console.log(cadena); // Imprime la cadena actual
+    console.log(longitud); // Imprime la longitud de la cadena
+
+    // Verificamos si la longitud actual es mayor que la mayor conocida
+    if (longitud > mayor) {
+        mayor = longitud;
+        posicion = i;
+    }
+}
+
+// Mostramos el resultado
+console.log('La mayor cantidad de caracteres tiene: ' + mayor);
+console.log('La CADENA con mayor cantidad de caracteres es: ' + miLista.lista[posicion]);
+
+console.log(miLista.lista); // Imprime: ['Hola', 'Mundo'm 'UCB']
+
 
         document.getElementById('agregarBtn').addEventListener('click', () => {
             const cadena = document.getElementById('cadena').value;
