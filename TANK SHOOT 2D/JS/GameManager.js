5f506bf5202297ac08d25797389563eda4886204
Alba Libera <alba.libera@ucb.edu.bo>
2024-10-18 10:21:38 -0400

    tank shoot

---
+++ TANK SHOOT 2D/JS/GameManager.js
@@ -0,0 +1,22 @@
+class GameManager{
+    puntuacion;
+    posY;
+    nivelActual;
+
+    constructor(_puntuacion, _nivelActual) {
+        this.puntuacion = _puntuacion;
+        this.nivelActual = _nivelActual;
+    }
+
+    reinicialNivel() {
+
+    }
+
+    actualizarPuntuacion() {
+
+    }
+
+    siguienteNivel() {
+
+    }
+}
